0c7028413a1a5f7d1f477ac545389254ea8ae942
teitei-tk <teitei.tk@gmail.com>
2021-11-08 02:25:22 +0900

    Inital Commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { defineComponent } from "vue";
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Editor from "@tinymce/tinymce-vue";
+const config = {
+  plugins: [
+    "advlist autolink lists link image charmap print preview anchor",
+    "searchreplace visualblocks code fullscreen",
+    "insertdatetime media table paste",
+  ],
+  toolbar:
+    "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
+  height: 400,
+};
+</script>
+
+<template>
+  <editor :init="config" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
